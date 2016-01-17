44dd1f69397da47f6958f0fe4c265af1404a8929
Brandon Aderholdt <aderholdtb@yahoo.com>
2016-01-17 10:29:19 -0800

    Update dealer.h

--- CountingCards/dealer.h
+++ CountingCards/dealer.h
@@ -10,11 +10,11 @@ const int maxTable = 5;
 
 class Dealer : public Player , public Deck{
  public:
-  Player p[maxTable];
+  Player p[maxTable];//TABLE OF PLAYERS
   int totalPlayers;
-  std::vector<std::string> aceDubs;
-  std::vector<std::string> reg;
-  int count;
+  std::vector<std::string> aceDubs;//RULES FOR ACES AND DOUBLES
+  std::vector<std::string> reg;//RULES FOR REGULAR HAND
+  int count;//CARD COUNT
   double avgWinnings;
   int winningHands;
 
@@ -23,9 +23,9 @@ class Dealer : public Player , public Deck{
   void DealCards();
   void nextPlayer();
   bool checkForShuffle();
-  int aceDoublesHand(bool, int, int);
-  int regular(int, int);
-  int play(std::string p);
+  int aceDoublesHand(bool, int, int);//CHECK RULES FOR GIVEN HAND, ACE OR PAIR HAND
+  int regular(int, int);//CHECK RULES FOR GIVEN HAND, REGUALR HAND
+  int play(std::string p);//GET PLAY TYPE
   void displayAllHands();
   void distributeWinnings(bool);
   void getNewCount();
